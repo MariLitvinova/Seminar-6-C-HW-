bcfb10dc004dc90c82dcda640b1e21976761aefc
Marina Litvinova <mari.s.litvinova@yandex.ru>
2022-12-26 21:31:32 +0300

    Изменена первая задача

--- Program.cs
+++ Program.cs
@@ -2,28 +2,23 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-// int [] CreateArray(int size){
-//     int [] array = new int [size];
-//     for (int i = 0; i<size; i++){
-//         Console.Write("Введите число: ");
-//         array[i] = Convert.ToInt32(Console.ReadLine());
-//     }
-//     Console.WriteLine();
-//     return array;
-// }
-// void Count (int [] array){
-//     int count = 0;
-//     for (int i = 0; i<array.Length; i++){
-//         if (array[i]> 0){
-//             count++;
-//         }
-//     }
-//         Console.WriteLine ($"Чисел больше нуля - {count}");
-// }
-//  Console.Write ("Введите количество чисел: ");
-//  int size = Convert.ToInt32(Console.ReadLine());
-//  int [] array = CreateArray (size);
-//  Count (array);
+int [] CreateArray(int size){
+    int [] array = new int [size];
+    int count = 0;
+    for (int i = 0; i<size; i++){
+        Console.Write("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i]>0){
+            count++;
+        }
+    }
+    Console.WriteLine ($"Чисел больше нуля - {count}");
+    return array;
+}
+ Console.Write ("Введите количество чисел: ");
+ int size = Convert.ToInt32(Console.ReadLine());
+ int [] array = CreateArray (size);
+
 
 // Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
